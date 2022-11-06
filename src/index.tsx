7d6da9196db469534b97502cb48c33a22e1f00d2
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-06 20:59:49 -0300

    Resolvendo rotas com js

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Inicio from './pages/Inicio';
+import Cardapio from './pages/Cardapio';
+
+const componentAtual = window.location.pathname === '/' ? <Inicio/> : <Cardapio/>;
 
 ReactDOM.render(
     <React.StrictMode>
-        <Inicio/>
+        {componentAtual}
     </React.StrictMode>,
     document.getElementById('root')
 );
