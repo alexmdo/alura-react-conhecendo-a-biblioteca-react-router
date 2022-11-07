09ce71303936a7b0f0e6743de06159b2ba8d2702
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-06 21:13:12 -0300

    Usando react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Inicio from './pages/Inicio';
-import Cardapio from './pages/Cardapio';
-
-const componentAtual = window.location.pathname === '/' ? <Inicio/> : <Cardapio/>;
+import Router from './routes';
 
 ReactDOM.render(
     <React.StrictMode>
-        {componentAtual}
+        <Router />
     </React.StrictMode>,
     document.getElementById('root')
 );
